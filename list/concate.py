f307f1df8d33647c20a4d0ccb5c84efc47fcd8a9
Manju9847 <bhusalmanju448@gmail.com>
2024-07-23 21:09:44 +0545

    concadination of lists

--- list/concate.py
+++ list/concate.py
@@ -3,8 +3,16 @@
 L1= [1,2,3,4]
 L2=[5,6,7,8]
 
-print("List1 before Concatenation:\n" + str(L1))
+# print("List1 before Concatenation:\n" + str(L1))
 for x in L2 : 
     L1.append(x) 
 
-print ("Concatenated list i.e. list1 after concatenation:\n" + str(L1))
+# print ("Concatenated list i.e. list1 after concatenation:\n" + str(L1))
+
+
+def merge(first_list,second_list):
+    for x in second_list : 
+        first_list.append(x) 
+    return(first_list)
+val=merge(L1,L2)
+print(val)

de85a696337f88fa8fe41d050a3615026dbe36e6
Manju9847 <bhusalmanju448@gmail.com>
2024-07-23 21:10:34 +0545

    list finding length of list

--- list/find_length.py
+++ list/find_length.py
@@ -1,10 +1,27 @@
 # Python | Ways to find length of list
 
-# count = 0
-# c = ["apple","ball","cat"]
-# for i in c:
-#     count=count+1
-# print(count)
+count = 0
+c = ["apple","ball","cat"]
+for i in c:
+    count=count+1
+print(count)
 
-l= [1,2,3,4]
-print(sorted(l))
+
+
+#also by using len()function
+# l= [1,2,3,4]
+# print(len(l))
+
+
+#using function
+
+def find_length(my_string):
+    count = 0    
+    for i in my_string:
+        count=count+1
+    return(count)
+
+animals = ["apple","ball","cat"]
+val= find_length(animals)
+print(val)
+
